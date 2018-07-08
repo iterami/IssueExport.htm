bedd1890141c38e8d2133517e9f1c86348321b83
Jan Hořava <czechinthemail@gmail.com>
2018-07-07 17:06:41 -0700

    changed var to let

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function fetch(){
-    var url = 'https://api.github.com/repos/'
+    let url = 'https://api.github.com/repos/'
       + document.getElementById('owner').value
       + '/'
       + document.getElementById('repo').value
@@ -10,11 +10,11 @@ function fetch(){
       + '&sort=' + document.getElementById('sort').value
       + '&state=' + document.getElementById('state').value;
 
-    var labels = document.getElementById('labels').value;
+    let labels = document.getElementById('labels').value;
     if(labels.length > 0){
         url += '&labels=' + labels;
     }
-    var since = document.getElementById('since').value;
+    let since = document.getElementById('since').value;
     if(since.length > 0){
         url += '&since=' + since;
     }
@@ -25,8 +25,8 @@ function fetch(){
 
           result_issues = JSON.parse(result);
 
-          var table_html = '';
-          for(var issue in result_issues){
+          let table_html = '';
+          for(let issue in result_issues){
               table_html += '<tr>'
                 + '<td>' + result_issues[issue]['number']
                 + '<td><a class=external href=' + result_issues[issue]['html_url'] + ' rel=noopener target=_blank>' + core_html_format({
